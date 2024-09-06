3c169ad016c68b7c39321f4813285f2cbb13c3e8
Viktoria Ritter <vika.ritter.03@gmail.com>
2024-09-06 16:47:11 +0500

    feat: add solution task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,20 @@
 class GuessingGame {
-    constructor() {}
-
     setRange(min, max) {
-
+        this.min = min + 1;
+        this.max = max - 1;
     }
 
     guess() {
-
+        this.value = Math.round((this.min + this.max) / 2);
+        return this.value;
     }
 
     lower() {
-
+        this.max = this.value - 1;
     }
 
     greater() {
-
+        this.min = this.value + 1;
     }
 }
 
